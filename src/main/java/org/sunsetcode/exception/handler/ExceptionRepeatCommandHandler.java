c4d2c40e25374b4c3c8441a582c783685beea54d
ghostlyit <supermaximal23@gmail.com>
2022-05-25 23:53:00 +0300

    Exception handlers and tests

--- src/main/java/org/sunsetcode/exception/handler/ExceptionRepeatCommandHandler.java
+++ src/main/java/org/sunsetcode/exception/handler/ExceptionRepeatCommandHandler.java
@@ -3,6 +3,7 @@ package org.sunsetcode.exception.handler;
 import org.sunsetcode.IoC.IoC;
 import org.sunsetcode.command.Command;
 import org.sunsetcode.command.RepeatCommand;
+import org.sunsetcode.command.RepeatTwiceCommand;
 
 import java.util.concurrent.BlockingQueue;
 
@@ -19,7 +20,12 @@ public class ExceptionRepeatCommandHandler implements Command
     public void execute()
     {
         var queue = IoC.<BlockingQueue<Command>>resolve("Command.Queue");
+
         Command repeatCommand = new RepeatCommand(command);
+        if (command instanceof RepeatCommand) {
+            repeatCommand = new RepeatTwiceCommand(command);
+        }
+
         queue.add(repeatCommand);
     }
 }
